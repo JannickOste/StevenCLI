8892928d42f40d85b59d9ab4da70df9e55a8680e
Jannick Oste <jannick@oste.dev>
2024-07-06 10:51:49 +0200

    chore: Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+import globals from "globals"
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['**/build/**', '**/dist/**', "sysadmin/**", "bin/**", "**/eslint.config.*"],
+  },
+  eslint.configs.recommended,
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: globals.node,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname
+      },
+    }, 
+    rules: {
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error',
+      '@typescript-eslint/consistent-return': 'error',
+      '@typescript-eslint/consistent-type-assertions': 'error',
+      '@typescript-eslint/ban-tslint-comment': "error",
+      "@typescript-eslint/no-array-constructor": "error",
+      //"@typescript-eslint/naming-convention": "error",
+      "no-unused-vars": "off",
+      '@typescript-eslint/no-unused-vars': 'error',
+      "no-undef": "off" 
+    },
+  },
+  {
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+   {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    ...tseslint.configs['recommended-requiring-type-checking'],
+  }
+);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Application from "./core/infrastructure/main";
+
+Application.main();
