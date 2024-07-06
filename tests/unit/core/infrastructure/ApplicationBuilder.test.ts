9ac0bbb42a7beb44ee0683cca6fb97e7d838de3b
Jannick Oste <jannick@oste.dev>
2024-07-06 21:24:21 +0200

    test(Application): tests updated for now structure

--- tests/unit/core/infrastructure/ApplicationBuilder.test.ts
+++ tests/unit/core/infrastructure/ApplicationBuilder.test.ts
@@ -9,7 +9,7 @@ import container from '../../../../src/core/infrastructure/di/DependencyContaine
 jest.mock('../../../../src/core/infrastructure/di/DependencyContainer', () => new Container());
 
 describe('ApplicationBuilder', () => {
-    let appBuilder: ApplicationBuilder; 
+    let appBuilder: ApplicationBuilder;
 
     beforeEach(() => {
         jest.clearAllMocks();
@@ -19,72 +19,88 @@ describe('ApplicationBuilder', () => {
 
     afterEach(() => {
         container.restore();
-    })
+    });
 
-    describe("setStartup", () => {
-        it('should bind startup to the Core.IAppStartup symbol type', () => {
-            appBuilder.setStartUp(class implements IStartup { async configureServices(){} async registerServices() {}})
-            
-            expect(container.isBound(TYPES.Core.IStartup)).toBe(true);
-        });
-
+    describe('setStartup', () => {
         it('should display a warning message when setStartUp is called twice or more', () => {
-            class MockStartup implements IStartup { async configureServices(){} async registerServices() {}}
+            class MockStartup implements IStartup { async configureServices(){} async registerServices() {} }
 
             console.warn = jest.fn();
             appBuilder.setStartUp(MockStartup)
                       .setStartUp(MockStartup);
-            
+
             expect(console.warn).toHaveBeenCalledTimes(1);
         });
     });
 
-    describe("build", () => {
-        it('should throw an error if Core.ICoreStartup is not bound', async () => {
-            await expect(appBuilder.build()).rejects.toThrow("No core application startup bound");
+    describe('build', () => {
+        it('should throw an error if no Core.IStartup is bound', async () => {
+            await expect(appBuilder.build()).rejects.toThrow();
         });
 
-        it('should call configureServices on only ICoreStartup if IAppStartup is not bound bound', async () => {
-            @injectable() 
-            class CoreStartup implements IStartup { async configureServices(){} async registerServices() {}}
+        it('should call registerServices and configureServices on the bound Core.IStartup', async () => {
+            @injectable()
+            class CoreStartup implements IStartup {
+                async configureServices() {}
+                async registerServices() {}
+            }
 
             const coreStartup = new CoreStartup();
 
-            const jestSpyCore = jest.spyOn(coreStartup, 'configureServices');
+            const registerSpy = jest.spyOn(coreStartup, 'registerServices');
+            const configureSpy = jest.spyOn(coreStartup, 'configureServices');
 
             container.bind(TYPES.Core.IStartup).toConstantValue(coreStartup);
 
             await appBuilder.build();
 
-            expect(jestSpyCore).toHaveBeenCalled();
+            expect(registerSpy).toHaveBeenCalled();
+            expect(configureSpy).toHaveBeenCalled();
         });
 
-        it('should call configureServices on both ICoreStartup and IAppStartup if they are bound', async () => {
-            @injectable() class CoreStartup implements IStartup { async configureServices(){} async registerServices() {}}
-            @injectable() class AppStartup implements IStartup { async configureServices(){} async registerServices() {}}
+        it('should call registerServices and configureServices on all bound IStartup instances', async () => {
+            @injectable() class CoreStartup implements IStartup {
+                async configureServices() {}
+                async registerServices() {}
+            }
+
+            @injectable() class AppStartup implements IStartup {
+                async configureServices() {}
+                async registerServices() {}
+            }
+
             const coreStartup = new CoreStartup();
             const appStartup = new AppStartup();
 
-            const jestSpyCore = jest.spyOn(coreStartup, 'configureServices');
-            const jestSpyApp = jest.spyOn(appStartup, 'configureServices');
+            const registerCoreSpy = jest.spyOn(coreStartup, 'registerServices');
+            const configureCoreSpy = jest.spyOn(coreStartup, 'configureServices');
+            const registerAppSpy = jest.spyOn(appStartup, 'registerServices');
+            const configureAppSpy = jest.spyOn(appStartup, 'configureServices');
 
             container.bind(TYPES.Core.IStartup).toConstantValue(coreStartup);
             container.bind(TYPES.Core.IStartup).toConstantValue(appStartup);
 
             await appBuilder.build();
 
-            expect(jestSpyCore).toHaveBeenCalled();
-            expect(jestSpyApp).toHaveBeenCalled();
+            expect(registerCoreSpy).toHaveBeenCalled();
+            expect(configureCoreSpy).toHaveBeenCalled();
+            expect(registerAppSpy).toHaveBeenCalled();
+            expect(configureAppSpy).toHaveBeenCalled();
         });
 
-         it('should resolve and return the Application instance', async () => {           
-            @injectable() class CoreStartup implements IStartup { async configureServices(){} async registerServices() {}}
+        it('should resolve and return the Application instance', async () => {
+            @injectable()
+            class CoreStartup implements IStartup {
+                async configureServices() {}
+                async registerServices() {}
+            }
+
             const coreStartup = new CoreStartup();
             container.bind(TYPES.Core.IStartup).toConstantValue(coreStartup);
-            
+
             const application = await appBuilder.build();
 
-            expect(application instanceof Application).toBeTruthy();
-         });
+            expect(application).toBeInstanceOf(Application);
+        });
     });
 });
