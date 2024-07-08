d1bc116cfb140c8307ba1b5cd50da8bb7560fd8c
Jannick Oste <jannick@oste.dev>
2024-07-08 16:55:56 +0200

    fix(CommandService): fixed appropriated towards tests

--- src/cli/infrastructure/services/CommandService.ts
+++ src/cli/infrastructure/services/CommandService.ts
@@ -1,4 +1,4 @@
-import { inject } from "inversify";
+import { inject, injectable } from "inversify";
 import ICommandService from "../../domain/services/ICommandService";
 import TYPES from "../../../TYPES";
 import ICommandRepository from "../../domain/repositories/ICommandRepository";
@@ -6,16 +6,22 @@ import ICommand from "../../domain/command/ICommand";
 import ICommandInfo from "../../domain/command/ICommandInfo";
 import { CommandMetadataKey } from "../../domain/command/decorators/Command";
 import ICommandConstructor from "../../domain/command/ICommandConstructor";
+import container from "../../../core/infrastructure/di/DependencyContainer";
+import CommandCollection from "../../domain/collections/CommandCollection";
+import "reflect-metadata"
+import ICommandMapper from "../../domain/mappers/ICommandMapper";
 
+@injectable()
 export default class CommandService implements ICommandService 
 {
     constructor(
-        @inject(TYPES.CLI.Repositories.ICommandRepository) private readonly repository: ICommandRepository
+        @inject(TYPES.CLI.Repositories.ICommandRepository) private readonly repository: ICommandRepository,
+        @inject(TYPES.CLI.mappers.ICommandMapper) private readonly mapper: ICommandMapper
     ) {
 
     }
 
-    getCommandInfo(command: new () => ICommand): ICommandInfo | undefined {
+    getCommandInfo(command: ICommandConstructor): ICommandInfo | undefined {
         const metadata: unknown = Reflect.getMetadata(CommandMetadataKey, command);
         if(!metadata || typeof metadata !== "object" || !("name" in metadata))
             return undefined;
@@ -23,34 +29,61 @@ export default class CommandService implements ICommandService
         return (metadata as ICommandInfo);
     }
 
-    getAll(): Promise<Array<ICommand>> {
-        return this.repository.getAll();
+    async getAll(): Promise<CommandCollection> {
+        return (await this.repository.getAll())
+                          .filter(
+                            (command) =>  "invoke" in command
+                                        && this.getCommandInfo(command.constructor as ICommandConstructor) !== undefined
+                        )
+    }
+
+    getChildByName(command: ICommandConstructor, name: string)
+    {
+        const info = this.getCommandInfo(command);
+        if(!info || !Array.isArray(info.children)) return undefined;
+
+        for(let child of info.children)
+        {
+            const childInfo = this.getCommandInfo(child);
+            if(!childInfo || childInfo.name !== name) continue;
+
+            return child;
+        }
+
+        return undefined;
     }
 
     async getCommandByName(name: string): Promise<ICommand | undefined> {
         
         if(typeof name === "string" && name.length)
+        {
+            const commands = this.mapper.collectionToNamedCollection(
+                await this.getAll()
+            )
+
+            if(commands[name] !== undefined) 
             {
-                const commands = (await this.getAll());
-                const nameSegments = name.split("/")
-    
+                return commands[name]
+            }
+
+            const nameSegments: string[] = name.split("/");
+            for(const [name, command] of Object.entries(commands))
+            {
+                if(name !== nameSegments[0]) continue;
+
+                nameSegments.shift()
+                let current: ICommandConstructor | undefined = command.constructor as ICommandConstructor;
                 do 
                 {
-                    const currentName = nameSegments.join("/")
-    
-                    for(let command of commands)
-                    {
-                        const currentCommandInfo = this.getCommandInfo(command.constructor as ICommandConstructor);
-                        if(currentCommandInfo?.name === currentName)
-                        {
-                            return command;
-                        }
-                    }
+                    const currentSegment = nameSegments.shift() ?? "";
                     
-                    nameSegments.pop()
-                } while(nameSegments.length)
+                    current = this.getChildByName(current, currentSegment)
+                } while(nameSegments.length && current)
+
+                return !current ? current : container.resolve(current);
             }
-    
-            return undefined;
+        }
+        
+        return undefined;
     }
 }
